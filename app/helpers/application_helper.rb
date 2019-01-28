84367def50423a4de2442f65cfc3ae9f55af816f
Patrick Dunn <Patrickdunn@patorikacBookea.lan>
2019-01-28 11:18:09 +1100

    Refactored alerts for flash, portfolio, and blog

--- app/helpers/application_helper.rb
+++ app/helpers/application_helper.rb
@@ -62,3 +62,19 @@ module ApplicationHelper
 		"active" if current_page? path
 	end
 end
+
+
+
+
+
+
+def alerts
+	alert = (flash[:alert] || flash[:error] || flash[:notice])
+	if alert
+		alert_generator alert	
+	end
+end
+
+def alert_generator msg
+	js add_gritter(msg, title: "Patrick Dunn Portfolio", sticky: false, time: 5000)  	
+end
